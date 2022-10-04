e925cf1814e3cd648911f29a18f4b49b97342094
olaf <olaf@DESKTOP-MKBROQU.localdomain>
2022-10-05 01:50:24 +0200

    updated issue

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,33 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     }
   },
-  build: {
 
-    target: "es2020",
-  }
+  build: {
+    target: "es2020"
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+        // Limit target browsers due to issue: Big integer literals are not available in the configured target environment ("chrome87", "edge88", "es2020", "firefox78", "safari13" + 2 overrides)'
+        target: "es2020",
+            // Node.js global to browser globalThis
+            define: {
+              global: 'globalThis'
+          },
+          // Enable esbuild polyfill plugins
+          plugins: [
+              NodeModulesPolyfillPlugin()
+          ]
+
+    }
+},
 })
